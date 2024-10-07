34fd98a40f381a47949387bb0c79346462e97b2b
Dlag545 <a_elnokrashy@u.pacific.edu>
2024-10-06 22:30:09 -0700

    A timer needs a measurement of time on which to increment (in this case 1000ms), an action to occur as the timer counts down, and a point at which the timer should stop

--- src/MyFirstTimer.java
+++ src/MyFirstTimer.java
@@ -1,10 +1,16 @@
 import acm.graphics.*;
 import acm.program.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MyFirstTimer extends GraphicsProgram {
+import javax.swing.*;
+
+public class MyFirstTimer extends GraphicsProgram implements ActionListener {
 	public static final int PROGRAM_HEIGHT = 600;
 	public static final int PROGRAM_WIDTH = 800;
 	public static final int MAX_STEPS = 20;
+	private int numTimes;
+	Timer someTimerVar = new Timer(1000, this);
 	private GLabel myLabel;
 
 	public void init() {
@@ -12,9 +18,23 @@ public class MyFirstTimer extends GraphicsProgram {
 		requestFocus();
 	}
 	
+	public void actionPerformed(ActionEvent e) {
+		numTimes++;
+		myLabel.setLabel("times called? " + numTimes);
+		myLabel.move(5, 0);
+		
+		if(numTimes >= 10) {
+			someTimerVar.stop();
+		}
+	}
+	
 	public void run() {
-		myLabel = new GLabel("# of times called?", 0, 100);
+		numTimes = 0;
+		myLabel = new GLabel("# of times called? ", 0, 100);
 		add(myLabel);
+		someTimerVar.setInitialDelay(3000);
+		someTimerVar.start();
+		
 	}
 	
 	public static void main(String[] args) {
